777ca0c71210a2039e941ce9161c9b3eeaba8767
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-26 14:35:25 -0500

    UpdateDOM function now runs the list in the console

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const completeList = document.getElementById('complete-list')
 const onHoldList = document.getElementById('on-hold-list')
 
 // Items
+let updatedOnLoad = false
 
 // Initialize Arrays
 let backlogListArray = []
@@ -34,8 +35,6 @@ function getSavedColumns() {
     onHoldListArray = ['Being uncool']
   }
 }
-getSavedColumns()
-updateSavedColumns()
 
 // Set localStorage Arrays
 function updateSavedColumns() {
@@ -65,9 +64,31 @@ function createItemEl(columnEl, column, item, index) {
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
   // Check localStorage once
+  if (!updatedOnLoad) {
+    getSavedColumns()
+  }
   // Backlog Column
+  backlogList.textContent = ''
+  backlogListArray.forEach((backlogItem, index) => {
+    createItemEl(backlogList, 0, backlogItem, index)
+  })
   // Progress Column
+  progressList.textContent = ''
+  progressListArray.forEach((progressItem, index) => {
+    createItemEl(progressList, 0, progressItem, index)
+  })
   // Complete Column
+  completeList.textContent = ''
+  completeListArray.forEach((completeItem, index) => {
+    createItemEl(completeList, 0, completeItem, index)
+  })
   // On Hold Column
+  onHoldList.textContent = ''
+  onHoldListArray.forEach((onHoldItem, index) => {
+    createItemEl(onHoldList, 0, onHoldItem, index)
+  })
   // Run getSavedColumns only once, Update Local Storage
 }
+
+// On Load
+updateDOM()
